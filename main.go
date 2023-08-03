05c80581d81b403af278d736c448c233fa479192
Sergei Mikhaltsov <s.mikhaltsov@leadgid.ru>
2023-08-03 16:20:45 +0300

    flags for num expected connections and timeout

--- main.go
+++ main.go
@@ -3,51 +3,51 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/shirou/gopsutil/net"
 	"os"
-	"strconv"
 	"time"
+
+	"github.com/shirou/gopsutil/net"
 )
 
 func main() {
-
 	portPtr := flag.Int("port", 8080, "port to check connections on")
+	expectedNumberConn := flag.Int("conn", 1, "expected number of connections")
+	workingTimeLimit := flag.Duration("t", 0, "timeout duration for the program (e.g., 30s or 1m)")
 	flag.Parse()
 
-	//logFile, err := os.OpenFile("/dev/fd/1", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//defer logFile.Close()
-	//log.SetOutput(logFile)
-
 	connCount := 0
+	startTime := time.Now()
+
 	for {
 		conns, err := net.Connections("tcp")
 		if err != nil {
 			fmt.Println("Error getting network connections:", err)
 			os.Exit(1)
 		}
+
 		for _, conn := range conns {
 			if conn.Laddr.Port == uint32(*portPtr) {
 				connCount++
 			}
 		}
-		//fmt.Fprintf(logFile, "Connections on port %d: %d\n", *portPtr, connCount)
+
 		fmt.Printf("Connections on port %d: %d\n", *portPtr, connCount)
-		if connCount <= 1 {
+
+		if connCount <= int(*expectedNumberConn) {
+			fmt.Println("Expected number of connections reached. Exiting.")
 			os.Exit(0)
 		}
+
 		connCount = 0
+
+		if *workingTimeLimit > 0 {
+			elapsed := time.Since(startTime)
+			if elapsed >= *workingTimeLimit {
+				fmt.Printf("Timeout of %v reached. Exiting.\n", *workingTimeLimit)
+				os.Exit(0)
+			}
+		}
+
 		time.Sleep(time.Second)
 	}
-}
-
-// atoi преобразует строку в целое число.
-func atoi(s string) int {
-	i, err := strconv.Atoi(s)
-	if err != nil {
-		panic(err)
-	}
-	return i
 }
